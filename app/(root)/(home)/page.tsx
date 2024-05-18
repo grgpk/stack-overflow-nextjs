4d00c2c998ef30318f6b2a076853d0c09a7d0078
Giorgi <grgpaikidze@gmail.com>
2024-05-19 00:07:30 +0400

    feat: add authentication with clerk

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,14 @@
+import { SignedIn, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <SignedIn>
+        <UserButton afterSignOutUrl="/" />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default Home;
